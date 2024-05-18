d3f8a0054d69b9f2fd1e05a2d4545c654051aba7
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-05-18 20:41:14 +0545

    feat: add integration test for default ahh execution

---
+++ tests/integration_tests.rs
@@ -0,0 +1,17 @@
+#[cfg(test)]
+mod ahh_integration {
+  use assert_cmd::Command;
+  use predicates::prelude::*;
+
+  #[test]
+  fn test_default_ahh_execution() {
+    let mut cmd = Command::cargo_bin("ahh").unwrap();
+
+    cmd
+      .args(&["When did the unix time start", "(Just the year)"])
+      .assert()
+      .success()
+      .stderr("")
+      .stdout(predicate::str::contains("1970"));
+  }
+}
